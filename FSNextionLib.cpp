25e1264225eebebb6404a301a125734eaec647d2
fsadak <fsadak@gmail.com>
2025-09-23 21:45:49 +0300

    Update FSNextionLib.cpp

--- FSNextionLib.cpp
+++ FSNextionLib.cpp
@@ -4,15 +4,15 @@
 FSNextionLib::FSNextionLib(HardwareSerial& serial) : _serial(serial) {}
 
 void FSNextionLib::begin(long baud, int8_t rxPin, int8_t txPin) {
-    #if defined(ESP32)
-        if (rxPin > -1 && txPin > -1) {
-            _serial.begin(baud, SERIAL_8N1, rxPin, txPin);
-        } else {
-            _serial.begin(baud);
-        }
-    #else
+#if defined(ESP32)
+    if (rxPin > -1 && txPin > -1) {
+        _serial.begin(baud, SERIAL_8N1, rxPin, txPin);
+    } else {
         _serial.begin(baud);
-    #endif
+    }
+#else
+    _serial.begin(baud);
+#endif
 }
 
 void FSNextionLib::endCommand() {
@@ -28,35 +28,12 @@ void FSNextionLib::sendCommand(const char* cmd) {
 
 void FSNextionLib::setText(const char* component, const char* txt) {
     String cmd = String(component) + ".txt=\"" + String(txt) + "\"";
-    _serial.print(cmd);
-    endCommand();
+    sendCommand(cmd.c_str());
 }
 
 void FSNextionLib::setNumber(const char* component, int value) {
     String cmd = String(component) + ".val=" + String(value);
-    _serial.print(cmd);
-    endCommand();
-}
-
-bool FSNextionLib::isConnected() {
-    while (_serial.available()) {
-        _serial.read();
-    }
-
-    _serial.print("get sleep");
-    endCommand();
-
-    long startTime = millis();
-    while (millis() - startTime < 300) {
-        if (_serial.available() > 0) {
-            while (_serial.available()) {
-                _serial.read();
-            }
-            return true;
-        }
-    }
-
-    return false;
+    sendCommand(cmd.c_str());
 }
 
 String FSNextionLib::getText(const char* component) {
@@ -99,13 +76,28 @@ int FSNextionLib::getNumber(const char* component) {
                 if (_serial.available() >= 7) {
                     byte buffer[4];
                     _serial.readBytes(buffer, 4);
-                    for(int i=0; i<3; i++) _serial.read();
+                    for (int i = 0; i < 3; i++) _serial.read();
                     return buffer[0] | (buffer[1] << 8) | (buffer[2] << 16) | (buffer[3] << 24);
                 }
             }
         }
     }
     return -1;
+}
+
+bool FSNextionLib::isConnected() {
+    while (_serial.available()) _serial.read();
+
+    sendCommand("get sleep");
+
+    long startTime = millis();
+    while (millis() - startTime < 300) {
+        if (_serial.available()) {
+            while (_serial.available()) _serial.read();
+            return true;
+        }
+    }
+    return false;
 }
 
 void FSNextionLib::onTouch(TouchEventCallback callback) {
@@ -117,9 +109,7 @@ void FSNextionLib::listen() {
 
     byte firstByte = _serial.read();
     if (firstByte != 0x65) {
-        while(_serial.available()) {
-            _serial.read();
-        }
+        while (_serial.available()) _serial.read();
         return;
     }
 
@@ -127,9 +117,7 @@ void FSNextionLib::listen() {
     byte componentId = _serial.read();
     byte eventType = _serial.read();
 
-    _serial.read();
-    _serial.read();
-    _serial.read();
+    _serial.read(); _serial.read(); _serial.read();
 
     if (_touchCallback) {
         _touchCallback(pageId, componentId, eventType);
@@ -146,6 +134,10 @@ String FSNextionLib::readRawNextionString(long timeout) {
         }
     }
     return result;
+}
+
+ComponentProxy FSNextionLib::operator[](const String& name) {
+    return ComponentProxy(this, name);
 }
 
 ComponentProxy FSNextionLib::operator[](const char* name) {

4e2f38b83ebb3305e0ccb20f335070f95b94a5d7
fsadak <fsadak@gmail.com>
2025-09-23 22:45:26 +0300

    Update ComponentProxy.h

--- ComponentProxy.h
+++ ComponentProxy.h
@@ -29,7 +29,6 @@ public:
     ComponentProxy& set(int value);
     ComponentProxy& update(int value);
     ComponentProxy& font(uint8_t fontId);
-    ComponentProxy& style(uint8_t styleId);
 
     void cmd(const String& raw);
 
